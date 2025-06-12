b0470da3608ad34bdd5449776f94e0d9b0dfd7d9
elm <elm@brreg.no>
2025-06-12 13:42:59 +0200

    Filtrering
    - satt opp filtrerings bokser fo kommune, orgform og konkurs status
    - lagt til stylig får å få det samlet på venstre side og unda søkefeltet
    - styling på h2

--- src/Oppslagsside.tsx
+++ src/Oppslagsside.tsx
@@ -1,22 +1,101 @@
 import React from 'react';
 import './Oppslagsside.css';
-import {Heading, Paragraph, Search} from "@digdir/designsystemet-react";
+import {
+    EXPERIMENTAL_MultiSuggestion,
+    EXPERIMENTAL_MultiSuggestionChips, EXPERIMENTAL_Suggestion, Field, Fieldset,
+    Heading, Label,
+    Paragraph,
+    Radio,
+    Search
+} from "@digdir/designsystemet-react";
 
 function Oppslagsside() {
-  return (
-    <>
-      <section>
-        <Heading level={1} data-size={"xl"}>Virksomhetsopplysninger</Heading>
-        <Paragraph data-size={"md"} className={'ingress'}>Her kan du gjører søk på virksomhetsnavn</Paragraph>
+    return (
+        <>
+            <section>
+                <Heading level={1} data-size={"xl"}>Virksomhetsopplysninger</Heading>
+                <Paragraph data-size={"md"} className={'ingress'}>Her kan du gjører søk på virksomhetsnavn</Paragraph>
 
-          <Search data-size={"md"}>
-              <Search.Input aria-label='søk' placeholder={'Her kan du søke på virskomhetsnavn'}/>
-              <Search.Clear/>
-              <Search.Button/>
-          </Search>
-      </section>
-    </>
-  );
+                <Search data-size={"md"}>
+                    <Search.Input aria-label='søk' placeholder={'Her kan du søke på virskomhetsnavn'}/>
+                    <Search.Clear/>
+                    <Search.Button/>
+                </Search>
+            </section>
+            <section className="filtrering-Wrapper">
+                <div className="filter-size">
+                    <Heading data-size={"md"}>Filtreringsmuligheter</Heading>
+                    <Field>
+                    <Label>
+                        Kommune
+                    </Label>
+                    <EXPERIMENTAL_MultiSuggestion className='filter-Wrapper' datatype={"sm"}>
+                        <EXPERIMENTAL_MultiSuggestionChips/>
+                        <EXPERIMENTAL_MultiSuggestion.Input/>
+                        <EXPERIMENTAL_MultiSuggestion.Clear/>
+                        <EXPERIMENTAL_MultiSuggestion.List>
+                            <EXPERIMENTAL_MultiSuggestion.Empty>
+                                Tomt
+                            </EXPERIMENTAL_MultiSuggestion.Empty>
+                            <EXPERIMENTAL_MultiSuggestion.Option value="Sogndal">
+                                Sogndal
+                                <div>
+                                    Kommune
+                                </div>
+                            </EXPERIMENTAL_MultiSuggestion.Option>
+                        </EXPERIMENTAL_MultiSuggestion.List>
+                    </EXPERIMENTAL_MultiSuggestion>
+                </Field>
+
+                <Field>
+                    <Label>
+                        Organisasjonsform
+                    </Label>
+                    <EXPERIMENTAL_MultiSuggestion className='filter-Wrapper' datatype={"sm"}>
+                        <EXPERIMENTAL_MultiSuggestionChips/>
+                        <EXPERIMENTAL_MultiSuggestion.Input/>
+                        <EXPERIMENTAL_MultiSuggestion.Clear/>
+                        <EXPERIMENTAL_MultiSuggestion.List>
+                            <EXPERIMENTAL_MultiSuggestion.Empty>
+                                Tomt
+                            </EXPERIMENTAL_MultiSuggestion.Empty>
+                            <EXPERIMENTAL_MultiSuggestion.Option value="ANNA">
+                                ANNA
+                                <div>
+                                    Annen juridisk peson
+                                </div>
+                            </EXPERIMENTAL_MultiSuggestion.Option>
+                        </EXPERIMENTAL_MultiSuggestion.List>
+                    </EXPERIMENTAL_MultiSuggestion>
+                </Field>
+
+                <Fieldset className='filter-Wrapper'>
+                    <Fieldset.Legend>Under Konkursbehandlig</Fieldset.Legend>
+                    <Radio label="Ja" value="value"/>
+                    <Radio label="Nei"/>
+                </Fieldset>
+
+                <Field>
+                    <Label>
+                        Dine filter
+                    </Label>
+                    <EXPERIMENTAL_Suggestion className='filter-Wrapper' datatype={"md"}>
+                        <EXPERIMENTAL_Suggestion.Input/>
+                        <EXPERIMENTAL_Suggestion.Clear/>
+                        <EXPERIMENTAL_Suggestion.List>
+                            <EXPERIMENTAL_Suggestion.Empty>
+                                Tomt
+                            </EXPERIMENTAL_Suggestion.Empty>
+                            <EXPERIMENTAL_Suggestion.Option value="BrønnøyMix">
+                                BrønnøyMix
+                            </EXPERIMENTAL_Suggestion.Option>
+                        </EXPERIMENTAL_Suggestion.List>
+                    </EXPERIMENTAL_Suggestion>
+                </Field>
+                </div>
+            </section>
+        </>
+    );
 }
 
 export default Oppslagsside;
