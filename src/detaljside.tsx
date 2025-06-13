4c3bb0bd1eeab7be5d1dc858cd9e0958a417c9ab
elm <elm@brreg.no>
2025-06-13 08:57:04 +0200

    Sette opp Auth0
    - lagt inn dependecie for auth0
    - komponenter for inn og ut logging
    - tatt i bruk knappene på begge sidene

--- src/detaljside.tsx
+++ src/detaljside.tsx
@@ -1,48 +1,65 @@
 import {Card, Heading, Paragraph} from "@digdir/designsystemet-react";
 import {DetailGroup, DetailRow} from "./components/detail-row";
 import './detaljside.css'
+import LogoutButton from "./Auth0/button-utlogging";
+import LoginButton from "./Auth0/button-innlogging";
+import React from "react";
+import {useAuth0} from "@auth0/auth0-react";
 
 export function Detaljside() {
 
+    const { user, isAuthenticated} = useAuth0();
+
     return (
-    <>
-        <Heading level={1} data-size={"xl"}>Navn på virksomhet</Heading>
-        <Paragraph>Orgnummer</Paragraph>
+        <>
+            <header>
+                {isAuthenticated ? (
+                    <div>
+                        <Paragraph>Innlogget: {user?.name}</Paragraph>
+                        <LogoutButton/>
+                    </div>
+                ) : (
+                    <LoginButton/>
+                )
+                }
+            </header>
+            <Heading level={1} data-size={"xl"}>Navn på virksomhet</Heading>
+            <Paragraph>Orgnummer</Paragraph>
 
-        <Card className='detalj-wrapper'>
-            <Card.Block>
-                <Heading level={2} data-size={"md"}>Opplysninger om virksomheten</Heading>
-            </Card.Block>
-            <Card.Block>
-                <DetailGroup>
-                    <DetailRow label='Foretningsadresse:'>
+            <Card className='detalj-wrapper'>
+                <Card.Block>
+                    <Heading level={2} data-size={"md"}>Opplysninger om virksomheten</Heading>
+                </Card.Block>
+                <Card.Block>
+                    <DetailGroup>
+                        <DetailRow label='Foretningsadresse:'>
 
-                    </DetailRow>
-                    <DetailRow label='Kommune, land:'>
+                        </DetailRow>
+                        <DetailRow label='Kommune, land:'>
 
-                    </DetailRow>
-                    <DetailRow label='Organisasjonsform:'>
+                        </DetailRow>
+                        <DetailRow label='Organisasjonsform:'>
 
-                    </DetailRow>
-                    <DetailRow label='under konkursbehandling:'>
+                        </DetailRow>
+                        <DetailRow label='under konkursbehandling:'>
 
-                    </DetailRow>
-                    <DetailRow label='Antall ansatte:'>
+                        </DetailRow>
+                        <DetailRow label='Antall ansatte:'>
 
-                    </DetailRow>
-                    <DetailRow label='Aktivitet:'>
+                        </DetailRow>
+                        <DetailRow label='Aktivitet:'>
 
-                    </DetailRow>
-                    <DetailRow label='Registreringsdato i Enhetsregisteret:'>
+                        </DetailRow>
+                        <DetailRow label='Registreringsdato i Enhetsregisteret:'>
 
-                    </DetailRow>
-                    <DetailRow label='Roller:'>
+                        </DetailRow>
+                        <DetailRow label='Roller:'>
 
-                    </DetailRow>
-                </DetailGroup>
+                        </DetailRow>
+                    </DetailGroup>
 
-            </Card.Block>
-        </Card>
-    </>
+                </Card.Block>
+            </Card>
+        </>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,23 @@ import './global.css';
 import '@digdir/designsystemet-css'
 import '@digdir/designsystemet-theme';
 import Oppslagsside from './Oppslagsside';
+import {Auth0Provider} from "@auth0/auth0-react";
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Oppslagsside />
-  </React.StrictMode>
+
+    <Auth0Provider
+        domain="dev-wu7dhnqsi37vzoke.us.auth0.com"
+        clientId="Q4yZNHwjw5cTH9TRXIVk19v0BN2H2hD5"
+        authorizationParams={{
+            redirect_uri: window.location.origin
+        }}
+    >
+        <React.StrictMode>
+            <Oppslagsside/>
+        </React.StrictMode>
+    </Auth0Provider>
 );
